54b5588a82a4949d53168a4b872e4e47ea570c41
loretta-dhahabu <loreydjefwa@gmail.com>
2022-08-19 19:53:05 +0300

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    attr_reader :title
 
+    def initialize(title="And Then There Were None")
+        @title=title
+    end
+    
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
